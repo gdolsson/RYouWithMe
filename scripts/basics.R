dcbff0d3d540f661b8cc9a83bb18c96d89459dc7
Gustaf Olsson <gustaf.olsson@lnu.se>
2021-06-28 15:57:15 +0200

    lesson 2 started

--- scripts/basics.R
+++ scripts/basics.R
@@ -13,8 +13,11 @@
 library(tidyverse)
 library(here)
 library(skimr)
+library(janitor)
+
 # Read in data ---------
 beaches <- read_csv(here("data", "sydneybeaches.csv"))
+
 # Exploring the data ---------
 View(beaches)
 dim(beaches)
@@ -23,9 +26,26 @@ glimpse(beaches)
 head(beaches)
 tail(beaches)
 summary(beaches)
+
 # Skimr ---------
 skim(beaches)
 
-select_all(beaches, toupper)
-select_all(beaches, tolower)
+# tidying columns ---------
+glimpse(beaches)
+
+select_all(beaches, toupper) # All column heads to upper
+select_all(beaches, tolower) # All column heads to lower
+
+clean_names(beaches) # Janitor, all to lower and changing the "Enterococci (cfu/100ml)" one
+
+names(beaches) # Nothing has changed, even though we've watched it change
+
+# If you want to actually change data, you must asign to a new object/variable
+cleanbeaches <- clean_names(beaches)
+names(cleanbeaches)
+View(cleanbeaches)
+# Rename, in cleanbeaches enterococci_cfu_100ml to beachbugs
+# newname = oldname
+cleanbeaches <- rename(cleanbeaches, beachbugs = enterococci_cfu_100ml)
+names(cleanbeaches)
 
